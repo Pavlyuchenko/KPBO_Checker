1d14fd4029cafe8aa0dd7a800865b13b99d68669
Pavlyuchenko <mmichal.pavlicek@seznam.cz>
2020-02-26 17:53:47 +0100

    Added templating to desktop app

--- Add_Books.py
+++ Add_Books.py
@@ -1,4 +1,34 @@
-import eel
+﻿import eel
+
+
+def get_books_from_file():
+    books = {}
+
+    file = open('C:\\Users\\Michal\\Desktop\\books.txt', 'r')
+
+    for line in file:
+        books[line.split(':')[0]] = line.split(':')[1] + ":" + line.split(':')[2]
+    return books
+
+
+template = open('C:\\Users\\Michal\\Desktop\\template.txt', 'r',encoding="utf-8")
+html = open('C:\\Users\\Michal\\Desktop\\result.html', 'w',encoding="utf-8")
+books = get_books_from_file()
+
+books_list = ""
+for key, value in books.items():
+    books_list += """
+    <tr>
+          <td>""" + key + """</td>
+          <td><a href=""" + value + " target='_blank'>" + value + """</td>
+    </tr>"""
+
+for line in template:
+    line = line.replace('{}', books_list)
+    html.write(line)
+
+template.close()
+html.close()
 
 eel.init('web')
 
@@ -11,5 +41,4 @@ def save_book(book_name, url):
     file.write('\n' + book_name + ":" + url)
     file.close()
 
-
-eel.start('index.html', size=(1000, 600))
+eel.start('result.html', size=(1000, 600), jinja_templates='web')
